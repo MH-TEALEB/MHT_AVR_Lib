777d3cdc74a20ff8037de43bc2356f0308d712f0
Mohamed Hossam Tealeb <MH.TEALEB31@GMAIL.COM>
2021-09-25 20:30:09 +0200

    Add files via upload

--- ATMEGA32/Projects/Traffic_Light_System/main.c
+++ ATMEGA32/Projects/Traffic_Light_System/main.c
@@ -1,12 +1,12 @@
 /*
- * @file Traffic_Light_System_1stMethod.c
+ * @file Traffic_Light_System.c
  *
  * @brief A Traffic light system project where The three lights(Red, Green and Yellow) are connected to 3 different pins in the same port,
  *		  and the time for each light is displayed in 2 7segments that are connected to 2 ports.
  *
  * @date	18 Sep. 2021 09:17:19 م
  * @author	MH_TEALEB
- */ 
+ */
 
 #ifndef	F_CPU
 	#define	F_CPU	1000000UL	// Define CPU frequency with 1MHz.
@@ -28,13 +28,13 @@ int main(void)
 	u8 _7SegNumbers[] = {0X3F, 0X06, 0X5B, 0X4F, 0X66, 0X6D, 0X7D, 0X07, 0X7F, 0X6F};	// Numbers of 7-segment from 0 to 9.
 	u8 led_on = RED_LED;	// Define "led_on" variable to choose which led to getting ON, and make RED led getting on at the first.
 	u8 time_on;				// Define "time_on" variable to hold the time in seconds for each led it will be ON.
-	
+
 	// Initialize the used ports for the two 7segments and leds to be output.
 	MHT_GPIO_PINSD_DIRECTION_DEFINE(0X7F);		// Make pins from 0 to 6 in port D as output for the 1st 7segment.
 	MHT_GPIO_PINSC_DIRECTION_DEFINE(0X7F);		// Make pins from 0 to 6 in port C as output for the 2nd 7segment.
 	MHT_GPIO_PINSA_DIRECTION_DEFINE(0X07);		// Make pins 0, 1, 2 in port A as output for the Green, Yellow and Red Lights.
-	
-    while (1) 
+
+    while (1)
     {
 		if (led_on == RED_LED){	// If its turn for Red led to be ON.
 			MHT_GPIO_PIN_OUTPUT_ONE(PA, RED_LED);
@@ -54,7 +54,7 @@ int main(void)
 			MHT_GPIO_PORTD_OUTPUT_VALUE(0X3F);
 			MHT_GPIO_PORTC_OUTPUT_VALUE(0X3F);
 		}
-		
+
 		// Loop to count the seconds that the led will be ON.
 		while (time_on > 0){
 			MHT_GPIO_PORTD_OUTPUT_VALUE(_7SegNumbers[(time_on % 10)]);
@@ -67,10 +67,10 @@ int main(void)
 		if(led_on > 2){
 			led_on = RED_LED;
 		}
-		
+
 	}	// End of while(1).
-	
+
 	return	0;	// Return 0 from main function.
-	
+
 }	// End of main Function.
 
